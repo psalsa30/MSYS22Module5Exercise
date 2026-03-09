4f1b3d82c2479f2b5ce81c689e0350f904e81958
psalsa30 <pauloualopsalvador@gmail.com>
2026-03-09 19:08:36 +0800

    initial commit

---
+++ tapas/tapasapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('basic_list', views.view_basic_list, name='view_basic_list'),
+    path('', views.view_menu, name='view_menu'),
+]
---
+++ tapas/tapasapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Dish
+
+# Create your views here.
+
+def view_basic_list(request):
+    dish_objects = Dish.objects.all()
+    return render(request, 'tapasapp/basic_list.html', {'dishes':dish_objects})
+
+
+def view_menu(request):
+    dish_objects = Dish.objects.all()
+    return render(request, 'tapasapp/list.html', {'dishes':dish_objects})
